85540f347ec20a5d28bca9feec707dd60f590474
Jakob <shavinski.jakob@gmail.com>
2023-04-07 16:57:58 -0700

    finish twoArrayObject function

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,33 @@
+"use strict";
+
+/*
+
+input: one array of keys, one array of values 
+output: object 
+
+if not enough values, the rest of keys are null
+if not enough keys, ignore the rest of the values 
+
+have an obj object that will store our key/values later on
+need to loop through our array of keys
+    we will assign key value pairs into our storage obj
+
+    if our second array (values) has fewer elements than our array (keys)
+    the array (keys) values will be null
+
+*/
+
 // add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+function twoArrayObject(keys, values) {
+    let storageKeysValues = {};
+
+    for (let i = 0; i < keys.length; i++) {
+        if (values[i] === undefined) {
+            storageKeysValues[keys[i]] = null;
+        } else {
+            storageKeysValues[keys[i]] = values[i];
+        }
+    }
+
+    return storageKeysValues;
 }
